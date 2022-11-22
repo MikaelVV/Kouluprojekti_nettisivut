db7e3b7e756ebd3d722bc3874d91c5c3a032f8a1
Mikael <50752113+MikaelVV@users.noreply.github.com>
2022-11-22 16:32:39 +0200

    Log in systeemin tekoa

---
+++ Web/login.js
@@ -0,0 +1,20 @@
+const objPeople = [
+    {
+        username: "Mikael",
+        password: "bob"
+    }
+]
+
+function getInfo(){
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+
+    for (i = 0; i < objPeople.length; i++){
+        if(username == objPeople [i].username && password == objPeople [i].password){
+            console.log(username + " on nyt kirjautunut sisään!")
+            return
+        }
+    }
+    console.log("Väärä käyttäjätunnus tai salasana!")
+    //console.log("olet nyt kirjautunut sisään! " + username + password)
+}
