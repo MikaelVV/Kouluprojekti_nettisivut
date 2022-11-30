3c797573165c6de7232f4a1f79c0f0e41d1b3956
Mikael <50752113+MikaelVV@users.noreply.github.com>
2022-11-30 17:24:45 +0200

    Hienosäätöä tilaushaun tekstin fontti koossa ja muuta

--- Web/tilaukset/orders.js
+++ Web/tilaukset/orders.js
@@ -12,19 +12,12 @@ fetch("https://www.cc.puv.fi/~asa/cgi-bin/fetchOrders.py")
                     <td>${values.respsalesperson}</td>
                     <td>${values.customerid}</td>
                     <td>${values.orderid}</td>
+                    <td>${values.totalprice}$</td>
                     <td>${values.comment}</td>
                     <td>${values.delivaddr}</td>
                     <td>${values.deliverydate}</td>
                     </tr>`;
     })
-        /*const markup = `<li>${customersearch.orderid}</li>`;
-        const markup2 = `<li>${customersearch.customerid}</li>`;
-        const markup3 = `<li>${customersearch.customer}</li>`;
-        
-
-        document.querySelector("ul").insertAdjacentHTML("beforeend", markup);
-        document.querySelector("ul").insertAdjacentHTML("beforeend", markup2);
-        document.querySelector("ul").insertAdjacentHTML("beforeend", markup3); */
         document.getElementById("table_body").innerHTML=tableData;
 
     
